059ce92c695e8ea3d28efc429db0d30084ed7c2b
Sarai <saray_temes@hotmail.com>
2021-12-16 22:54:50 +0100

    funciones de los botones de mostrar

--- script.js
+++ script.js
@@ -1,11 +1,22 @@
+function mostrarCuenta(){
+   var iban = document.getElementById("iban").value;
+   var entity = document.getElementById("entity").value;
+   var sucursal = document.getElementById("sucursal").value;
+   var dc = document.getElementById("dc").value;
+   var cuenta = document.getElementById("account").value;
+            
+   alert("Le informamos que su cuenta bancaria es: ES " + "-" + iban + "-" + entity + "-" + sucursal + "-" + dc + "-" + cuenta);
 
-    function mostrarCuenta(){
-            
-            var iban = document.getElementById("iban").value;
-            var entity = document.getElementById("entity").value;
-            var sucursal = document.getElementById("sucursal").value;
-            var dc = document.getElementById("dc").value;
-            var cuenta = document.getElementById("account").value;
-            
-            alert("Le informamos que su cuenta bancaria es: ES " + "-" + iban + "-" + entity + "-" + sucursal + "-" + dc + "-" + cuenta);
-        }
+}
+
+
+function mostrarDia() {
+
+   var dia = document.getElementById('date');
+   var dia = new Date;
+   var n = dia.getDay();
+   var dias = new Array("Domingo","Lunes","Martes","Miércoles","Jueves","Viernes","Sábado");
+   alert('La fecha seleccionada en el elemento de fecha es un ' + dias[n]);
+
+
+  }
